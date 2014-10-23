eba5bf9c8e11f20308c060358cfe86d361c7827b
kukhelny <kukhelny@gmail.com>
2014-10-24 01:11:15 +0300

    Initial Commit

---
+++ AppleHotNews/AHNAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AHNAppDelegate.h
+//  AppleHotNews
+//
+//  Created by Seva Kukhelny on 24.10.14.
+//  Copyright (c) 2014 Seva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AHNAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AppleHotNews/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AppleHotNews
+//
+//  Created by Seva Kukhelny on 24.10.14.
+//  Copyright (c) 2014 Seva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AHNAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AHNAppDelegate class]));
+    }
+}
